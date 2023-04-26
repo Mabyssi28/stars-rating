f177ca6ee22ca5176014e215922f5aa299ba2f33
Josué Rivera Henao <josu.riverah@gmail.com>
2023-04-26 18:29:02 -0500

    feat: new version 1.1.0

--- js/starsRating.js
+++ js/starsRating.js
@@ -2,89 +2,109 @@ class StarsRating {
   constructor(refId, options) {
     this.refId = refId;
     this.options = options;
-    this.stars = this.options?.stars || this.getStarsDefault();
-    this.value = 0;
+    this.stars = options?.stars || StarsRating.getStarsDefault();
+    this.value = options?.value || StarsRating.getDefaultValue();
+    this.isDisabled = options?.isDisabled || StarsRating.getDefaultIsDisabled();
 
     this.init();
   }
 
-  init = () => {
+  init() {
     this.setContainer();
     this.setStars();
-    this.setEventsStars();
+
+    if (!this.isDisabled) {
+      this.setEventsStars();
+    } else {
+      this.fillStars(this.value);
+    }
   }
 
-  setContainer = () => {
+  setContainer() {
     this.container = document.getElementById(this.refId);
   }
 
-  getContainer = () => {
+  getContainer() {
     return this.container;
   }
 
-  setStars = () => {
+  setStars() {
     for (let i = 1; i <= this.stars; i++) {
-      const stars = document.createElement('span');
-      stars.className = 'star';
+      const star = document.createElement('span');
+      star.className = 'star';
 
       if (this.options?.size) {
-        const size = this.options?.size;
+        const size = this.options.size;
 
-        stars.style.setProperty('--stars-rating--width', `${size}px`);
-        stars.style.setProperty('--stars-rating--height', `${size}px`);
-        stars.style.setProperty('--stars-rating--font-size', `${size}px`);
+        star.style.setProperty('--stars-rating--width', `${size}px`);
+        star.style.setProperty('--stars-rating--height', `${size}px`);
+        star.style.setProperty('--stars-rating--font-size', `${size}px`);
+      }
+
+      if (this.isDisabled) {
+        star.style.setProperty('--stars-rating--cursor', 'default');
       }
 
       if (this.options?.colorEmpty) {
-        stars.style.setProperty('--stars-rating--color-empty', this.options.colorEmpty);
+        star.style.setProperty('--stars-rating--color-empty', this.options.colorEmpty);
       }
 
       if (this.options?.colorFilled) {
-        stars.style.setProperty('--stars-rating--color-filled', this.options.colorFilled);
+        star.style.setProperty('--stars-rating--color-filled', this.options.colorFilled);
       }
 
-      this.container.appendChild(stars);
-      this.starsDom = this.container.querySelectorAll('.star');
+      this.container.appendChild(star);
     }
+
+    this.starsDom = this.container.querySelectorAll('.star');
   }
 
-  setEventsStars = () => {
-    const fillStars = (targetIndex) => {
-      this.starsDom.forEach((star, index) => {
-        if (index <= targetIndex) {
-          star.classList.add('filled');
-        } else {
-          star.classList.remove('filled');
-        }
-      });
-    }
-
-    // const resetStars = () => {
-    //   this.starsDom.forEach(star => {
-    //     star.classList.remove('filled');
-    //   });
-    // }
-    
-    const setRating = (event) => {
-      const targetIndex = parseInt(event.target.getAttribute('data-index'));
-      this.value = targetIndex + 1;
-
-      // fillStars(targetIndex);
-    }
-
+  fillStars(targetIndex) {
     this.starsDom.forEach((star, index) => {
-      star.addEventListener('mouseover', (e) => fillStars(parseInt(e.target.getAttribute('data-index'))));
-      // star.addEventListener('mouseout', resetStars);
-      star.addEventListener('click', setRating);
+      star.classList.toggle('filled', index < targetIndex);
+    });
+  }
+
+  resetStars() {
+    this.starsDom.forEach(star => {
+      star.classList.remove('filled');
+    });
+  }
+
+  setRating(event) {
+    const targetIndex = parseInt(event.target.getAttribute('data-index'));
+    let valueTemp = targetIndex + 1;
+
+    if (valueTemp === this.value) {
+      this.resetStars();
+      valueTemp = StarsRating.getDefaultValue();
+    } else {
+      this.fillStars(valueTemp);
+    }
+
+    this.value = valueTemp;
+  };
+
+  setEventsStars() {
+    this.starsDom.forEach((star, index) => {
+      star.addEventListener('click', (event) => this.setRating(event));
       star.setAttribute('data-index', index);
     });
   }
 
-  getValue = () => {
-    return this.value;
+  static getDefaultValue() {
+    return 0;
   }
 
-  getStarsDefault = () => {
+  static getStarsDefault() {
     return 5;
+  }
+
+  static getDefaultIsDisabled() {
+    return false;
+  }
+
+  getValue() {
+    return this.value;
   }
 }
